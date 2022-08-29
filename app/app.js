64b76842810591cb724ba53889233464aa49fe4e
Tuuli Rasimus <tuuli.rasimus@gmail.com>
2022-08-29 15:53:16 +0300

    submitted part 2.1

--- app/app.js
+++ app/app.js
@@ -2,8 +2,8 @@ import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
 const handleRequest = (request) => {
   console.log(`Request to ${request.url}`);
-  return new Response("Hello world!");
+  return new Response("I will learn how to write web applications!");
 };
 
-console.log("Launching server on port 7777");
-serve(handleRequest, { port: 7777 });
+console.log("Launching server on port 31337");
+serve(handleRequest, { port: 31337 });
