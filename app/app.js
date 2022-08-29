ec9105f2d22d455e91c8148049c668454447894c
Tuuli Rasimus <tuuli.rasimus@gmail.com>
2022-08-29 19:06:53 +0300

    submitted 3.3 ex 1

--- app/app.js
+++ app/app.js
@@ -1,7 +1,11 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
 const handleRequest = (request) => {
-  return Response("Hi all!");
+  const url = new URL(request.url);
+  const path = url.pathname;
+  const message = `You requested the path ${path}`;
+
+  return new Response(message);
 };
 
 serve(handleRequest, { port: 7777 });
