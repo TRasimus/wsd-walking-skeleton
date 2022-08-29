35e3991d0b70867df5c6051a1088dcdb55038ac9
Tuuli Rasimus <tuuli.rasimus@gmail.com>
2022-08-29 16:11:47 +0300

    answered part 3.1. questions

--- app/app.js
+++ app/app.js
@@ -1,9 +1,7 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
 const handleRequest = (request) => {
-  console.log(`Request to ${request.url}`);
-  return new Response("I will learn how to write web applications!");
+  return Response("Hi all!");
 };
 
-console.log("Launching server on port 31337");
-serve(handleRequest, { port: 31337 });
+serve(handleRequest, { port: 7777 });
